42820ab9d47f148855908863d0b7c2d4ce256541
ewraan septian nur R <erwan_ramadhan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:58:43 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,7 @@
+public class tes {
+    public static void main(String[] args) {
+          System.out.println("nama saya Erwan septian N.R dari kelas XRPL7 yang berasal dari kota Tulungagung");
+          String a = "1";
+          
+    }
+}
